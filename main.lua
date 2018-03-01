2d4be012a83e23a56c1a59a3316205bc60e4edc6
Aliya Satifa <aliya.satifa@ocsbstudent.ca>
2018-03-01 10:46:46 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,6 @@
+-- Title: MovingObjects
+-- Name: Aliya Satifa
+-- Course: ICS2O/3C
+-- This program moves a beetleship moving
+-- across the screen as weell as another object moving in a differant direction
+print()
